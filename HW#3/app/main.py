4e8bcb08248f717c3d93ab67614ce44304b107b4
Jihyeon Lee <101785677+wlgus5704@users.noreply.github.com>
2022-05-28 10:56:53 +0900

    Add files via upload
    html + python FIN

---
+++ HW#3/app/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')  # default URLpyt
+def student():
+   return render_template('input_info.html')
+
+  
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = dict()
+      result['Name'] = request.form.get('Name')
+      result['Student Number'] = request.form.get('Student Number')
+      result['Gender'] = request.form.get('Gender')
+      result['Major'] = request.form.get('Major')
+
+      txt = ', '.join( i for i in request.form.getlist('Programming Languages'))
+      result['Programming Languages'] = txt
+      
+      return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+   app.run()
